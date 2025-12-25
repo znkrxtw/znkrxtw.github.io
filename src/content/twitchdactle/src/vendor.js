df4d2d654be1e430fd68281026bfb75406630b49
GerdNer <randomdevofgames@googlemail.com>
2025-12-25 09:09:09 +0100

    replaced comfy.js

--- src/content/twitchdactle/src/vendor.js
+++ src/content/twitchdactle/src/vendor.js
@@ -1,5 +1,7 @@
 // Import npm libraries and expose them globally for existing scripts
 import baffle from 'baffle';
+import comfy from 'comfy.js';
 
 // Expose baffle globally
 window.baffle = baffle;
+window.comfy = comfy;
